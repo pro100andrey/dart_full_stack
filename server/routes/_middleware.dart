1373ca49a793308747922f20832e19c398a58e59
Andrey Ivanov <this.andrey@gmail.com>
2024-04-22 13:04:57 +0300

    wip bearerAuthentication

--- server/routes/_middleware.dart
+++ server/routes/_middleware.dart
@@ -1,4 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_auth/dart_frog_auth.dart';
 import 'package:prisma_projects_data_source/data_source.dart';
 import 'package:projects_data_source/projects_data_source.dart';
 
@@ -9,5 +10,6 @@ final _projectsDataSource = PrismaProjectsBaseDataSource(
 Handler middleware(Handler handler) {
   return handler
       .use(requestLogger())
-      .use(provider<ProjectsDataSource>((_) => _projectsDataSource));
+      .use(provider<ProjectsDataSource>((_) => _projectsDataSource))
+      .use(bearerAuthentication());
 }
